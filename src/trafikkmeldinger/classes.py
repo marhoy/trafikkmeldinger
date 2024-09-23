42e11dbbc491abe17692c2abb5e1b525e664e3b2
Martin Høy <martin@hoy.priv.no>
2024-09-23 16:23:01 +0200

    - Add ability to read Police log - Make ABC for Thread and Message

--- src/trafikkmeldinger/classes.py
+++ src/trafikkmeldinger/classes.py
@@ -1,99 +1,66 @@
 """Define classes for trafikkmeldinger package."""
 
+from __future__ import annotations
+
 import datetime
-import re
+from abc import ABC, abstractmethod
 from enum import IntEnum
-from os.path import commonprefix
 
 from pydantic import BaseModel
 
 
 class Status(IntEnum):
-    """Status of a conversation."""
+    """Status of a thread."""
 
     NEW = 1
     FIXING = 2
     DONE = 3
 
 
-class Tweet(BaseModel):
-    """A tweet from Twitter / X."""
-
-    id: int
-    conversation_id: int
-    created_at: datetime.datetime
-    text: str
-
-
 class Message(BaseModel):
-    """A message in a conversation."""
+    """A message in a thread."""
 
     created_at: datetime.datetime
     text: str
 
+    def __lt__(self, other: Message) -> bool:
+        """Messages can be sorted by creation time."""
+        return self.created_at < other.created_at
 
-def upperfirst(string: str) -> str:
-    """Uppercase first character of string, leave rest unchanged."""
-    return string[:1].upper() + string[1:]
 
+class Thread(ABC):
+    """A thread of messages."""
 
-class Conversation:
-    """A conversation consisting of multiple messages."""
+    @property
+    @abstractmethod
+    def created_at(self) -> datetime.datetime:
+        """The creation time of the thread."""
+        pass
 
-    def __init__(self, tweet: Tweet) -> None:
-        """Initialize a conversation with a tweet."""
-        self.created_at = tweet.created_at
-        self.updated_at = tweet.created_at
-        self.tweets: list[Tweet] = [tweet]
-        self.status = Status.NEW
-        self.location = ""
-        self.messages: list[Message] = []
-        self.update()
+    @property
+    @abstractmethod
+    def status(self) -> Status:
+        """The status of the thread."""
+        pass
 
-    def add_tweet(self, tweet: Tweet) -> None:
-        """Add a tweet to the conversation."""
-        self.updated_at = tweet.created_at
-        self.tweets.append(tweet)
-        self.update()
+    @property
+    @abstractmethod
+    def location(self) -> str:
+        """The location of the thread."""
+        pass
 
-    def update(self) -> None:
-        """Update status, location and messages."""
-        self.update_status()
-        self.update_location_and_messages()
+    @property
+    @abstractmethod
+    def updated_at(self) -> datetime.datetime:
+        """The last update time of the thread."""
+        pass
 
-    def update_status(self) -> None:
-        """Update conversation status based on tweets."""
-        for tweet in self.tweets:
-            if any(word in tweet.text.lower() for word in ("på stedet", "berging")):
-                self.status = Status.FIXING
-            if any(
-                word in tweet.text.lower()
-                for word in ("åpen", "åpnet", "åpne", "fjernet", "ryddet", "sjekket")
-            ):
-                self.status = Status.DONE
+    @property
+    @abstractmethod
+    def messages(self) -> list[Message]:
+        """The messages in the thread."""
+        pass
 
-    def update_location_and_messages(self) -> None:
-        """Exctract location string from messages and remove it from all messages."""
-        if len(self.tweets) == 1:
-            # If there is only one tweet, assume location is the first substring that
-            # ends with either of .:; (and then something else than a number, to avoid
-            # splitting on 'Rv. 3')
-            if match := re.match(r"^(.+?)[.:;]\s+\D", self.tweets[0].text):
-                prefix = match.group(1)
-            else:
-                prefix = ""
-        else:
-            # If there are multiple tweets, assume the common prefix is the location.
-            prefix = commonprefix([tweet.text for tweet in self.tweets])
-            if match := re.match(r"^(.+?)[.:;]\s+\D", prefix):
-                # Include up to the last ,.:;
-                prefix = match.group(1)
-
-        self.location = prefix.rstrip(" ,:;.").replace("#", "")
-        self.messages = [
-            Message(
-                text=upperfirst(tweet.text.removeprefix(prefix).lstrip(" ,:;.")),
-                created_at=tweet.created_at,
-            )
-            for tweet in self.tweets
-        ]
+    def __lt__(self, other: Thread) -> bool:
+        """Threads can be sorted by creation time."""
+        return self.created_at < other.created_at
